d871c969ca2053174674fd5b8365686d187aa88d
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-04-24 20:52:26 +0200

    Pair programming

---
+++ index.js
@@ -0,0 +1,40 @@
+// Pair Programming
+// Variables
+let variableSinValor;
+let booleano1 = false;
+let booleano2 = true;
+
+const PI = 3.14;
+const TAU = 2 * PI;
+
+// Booleanos
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 =
+  (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+
+// Bucles
+let contarHasta10_2 = 0;
+for (contarHasta10_2; contarHasta10_2 < 10; contarHasta10_2++);
+
+let postI = 0;
+let postJ = 0;
+for (let i = 0; i < 11; i++) {
+  postI += postJ++;
+}
+
+let sumaPares = 0;
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) sumaPares += i;
+}
+
+// Proyecto Individual
+let variableValorNumerico = 15;
+const MiNombre = "Zuhaitz";
+const MiNumeroFav = 8;
