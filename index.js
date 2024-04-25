02059524e094eb0658ac6a89dc400922f180b958
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-04-25 12:22:39 +0200

    Exercise complete

--- index.js
+++ index.js
@@ -35,6 +35,36 @@ for (let i = 0; i < 10; i++) {
 }
 
 // Proyecto Individual
+// Variables
 let variableValorNumerico = 15;
 const MiNombre = "Zuhaitz";
 const MiNumeroFav = 8;
+
+// Booleanos
+let booleanoOr = booleano1 || booleano2;
+let booleanoMix1 =
+  (booleano1 && TAU / 2 === PI) || variableValorNumerico >= MiNumeroFav;
+let seisNoEsNueve = 6 !== 9;
+let booleanoMix2 =
+  variableValorNumerico > 0 || variableValorNumerico < -(MiNumeroFav / TAU);
+
+// Operaciones
+let valorSuma = MiNumeroFav + variableValorNumerico;
+let valorResta = MiNumeroFav - variableValorNumerico;
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+let valorDivision = MiNumeroFav / 3;
+
+// Bucles
+let contarHasta10 = 0;
+while (contarHasta10 < 10) contarHasta10++;
+
+let preI = 0;
+let preJ = 0;
+for (let i = 0; i < 11; i++) {
+  preI += ++preJ;
+}
+
+let sumaImpares = 0;
+for (let i = 0; i < 10; i++) {
+  if (i % 2 !== 0) sumaImpares += i;
+}
